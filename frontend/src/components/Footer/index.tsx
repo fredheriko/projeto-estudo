84fdcf627b0a70e543f037c47b5637e16621c540
fredheriko <fredheriko@hotmail.com>
2021-09-10 17:13:44 -0300

    Componentes estaticos básicos

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,12 @@
+const Footer = () => {
+  return (
+    <footer className="footer mt-auto py-3 bg-dark">
+      <div className="container">
+        <p className="text-light">App desenvolvido por <a href="https://www.linkedin.com/in/frederico-passos-bezerra/" target="_blank" rel="noreferrer">Frederico Bezerra</a></p>
+        <p className="text-light"><small><strong>Semana Spring React</strong><br />
+          Evento promovido pelo generoso e genial Nelio Alves: <a href="https://instagram.com/devsuperior.ig" target="_blank" rel="noreferrer">@devsuperior.ig</a></small></p>
+      </div>
+    </footer>
+  );
+}
+export default Footer;
