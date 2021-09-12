a074310e3c246cf4fa029a0942c7b50c5a24e9a5
fredheriko <fredheriko@hotmail.com>
2021-09-12 01:54:46 -0300

    Easter egg

--- frontend/src/components/DataTable/index.tsx
+++ frontend/src/components/DataTable/index.tsx
@@ -14,26 +14,26 @@ const DataTable = () => {
         <tbody>
           <tr>
             <td>22/04/2021</td>
-            <td>Barry Allen</td>
+            <td>Charles Babbage</td>
             <td>34</td>
             <td>25</td>
             <td>15017.00</td>
           </tr>
           <tr>
             <td>22/04/2021</td>
-            <td>Barry Allen</td>
+            <td>Paul Baran</td>
             <td>34</td>
             <td>25</td>
             <td>15017.00</td>
           </tr><tr>
             <td>22/04/2021</td>
-            <td>Barry Allen</td>
+            <td>Linus Torvalds</td>
             <td>34</td>
             <td>25</td>
             <td>15017.00</td>
           </tr><tr>
             <td>22/04/2021</td>
-            <td>Barry Allen</td>
+            <td>James Gosling</td>
             <td>34</td>
             <td>25</td>
             <td>15017.00</td>
